11cd4be3d9cf5d424ce0b0385157039672b3f898
lukedigiovanna <lukedigiovanna@gmail.com>
2021-05-13 15:35:19 -0400

    linear regression demo

--- Physics Simulations and AI/linreg.cpp
+++ Physics Simulations and AI/linreg.cpp
@@ -2,6 +2,30 @@
 
 #include <iostream>
 
-void test() {
-	std::cout << "Hello " << std::endl;
+void LeastSquaresRegression::fit(float data[][2], int n) {
+	const float lr = 0.01f; // learning rate
+	for (int k = 0; k < 1000; k++) {
+		// calculate the current gradient
+		float grIntercept = 0.0f, grSlope = 0.0f;
+		for (int i = 0; i < n; i++) {
+			grIntercept += predict(data[i][0]) - data[i][1];
+			grSlope += (predict(data[i][0]) - data[i][1]) * data[i][0];
+		}
+		printf("gradient = <%f, %f> y = %f + %fx cost = %f\n", grIntercept, grSlope, intercept, slope, getCost(data, n));
+		slope -= lr * grSlope;
+		intercept -= lr * grIntercept;
+	}
+}
+
+float LeastSquaresRegression::predict(float x) const {
+	return intercept + x * slope;
+}
+
+float LeastSquaresRegression::getCost(float data[][2], int n) {
+	float cost = 0;
+	for (int i = 0; i < n; i++) {
+		float residual = predict(data[i][0]) - data[i][1];
+		cost += residual * residual;
+	}
+	return cost;
 }
--- Physics Simulations and AI/linreg.h
+++ Physics Simulations and AI/linreg.h
@@ -1,3 +1,14 @@
 #pragma once
 
-void test();
+class LeastSquaresRegression {
+private:
+	float slope, intercept;
+public:
+	LeastSquaresRegression() : slope(0), intercept(0) {}
+
+	void fit(float data[][2], int n); // fits the model to the data
+
+	float predict(float x) const;
+
+	float getCost(float data[][2], int n);
+};
